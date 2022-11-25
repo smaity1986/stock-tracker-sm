fdacf2192b0d1050e38e7378256b8092f4ec898b
smaity1986 <119071792+smaity1986@users.noreply.github.com>
2022-11-26 01:07:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stock-tracker-sm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stock-tracker-sm)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { MainComponent } from './component/main/main.component';
+import { ListComponent } from './component/list/list.component';
+import { SearchComponent } from './component/search/search.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MainComponent,
+    ListComponent,
+    SearchComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
